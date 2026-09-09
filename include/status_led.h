acb16eb54cc0338d087914a9518f65eaccd51a86
FogboundSloth25 <108718835+FogboundSloth25@users.noreply.github.com>
2026-09-09 12:58:22 +0300

    Add transition LED state

--- include/status_led.h
+++ include/status_led.h
@@ -5,3 +5,4 @@ void recovery_led_set_rgb(uint8_t r, uint8_t g, uint8_t b);
 void recovery_led_waiting(void);
 void recovery_led_detected(void);
 void recovery_led_dfu(void);
+void recovery_led_transition(void);
