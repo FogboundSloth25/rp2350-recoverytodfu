75f94a33436ea999ed4068e851b35fe15ac3c43c
FogboundSloth25 <108718835+FogboundSloth25@users.noreply.github.com>
2026-09-09 12:26:11 +0300

    Fix status LED API naming

--- include/status_led.h
+++ include/status_led.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
-void status_led_init(void);
-void status_led_set_rgb(uint8_t r,uint8_t g,uint8_t b);
-void status_led_waiting(void);
-void status_led_detected(void);
-void status_led_dfu(void);
+void recovery_led_init(void);
+void recovery_led_set_rgb(uint8_t r, uint8_t g, uint8_t b);
+void recovery_led_waiting(void);
+void recovery_led_detected(void);
+void recovery_led_dfu(void);
